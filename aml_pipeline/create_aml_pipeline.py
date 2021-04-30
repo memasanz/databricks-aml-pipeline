37e29048af3fa89e6b39a69c0ad38f540469878a
memasanz <66378877+memasanz@users.noreply.github.com>
2021-04-30 17:44:03 -0500

    Update create_aml_pipeline.py

--- aml_pipeline/create_aml_pipeline.py
+++ aml_pipeline/create_aml_pipeline.py
@@ -141,7 +141,7 @@ def main():
     # Step 1, Run the data prep script
     prep_step = PythonScriptStep(name = "Prepare Data",
                                     script_name = "prep_diabetes.py",
-                                    source_directory = '.',
+                                    source_directory = e.source_directory,
                                     arguments = ['--input-data', diabetes_ds.as_named_input('raw_data'),
                                                  '--prepped-data', prepped_data_folder],
                                     outputs=[prepped_data_folder],
@@ -151,7 +151,7 @@ def main():
 
     # Step 2, run the training script
     train_step = PythonScriptStep(name = "Train and Register Model",
-                                    source_directory = '.',
+                                    source_directory =  e.source_directory,
                                     script_name = "train_diabetes.py",
                                     arguments = ['--training-folder', prepped_data_folder],
                                     inputs=[prepped_data_folder],
